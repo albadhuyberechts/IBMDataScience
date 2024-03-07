3c308deb060d8379b1316d82089511f71cc593bc
albadhuyberechts <19596921+albadhuyberechts@users.noreply.github.com>
2024-03-07 11:39:45 +0100

    Add files via upload

---
+++ IBM_Final/SpaceX_Machine_Learning_Prediction_Part_5.py
@@ -0,0 +1,193 @@
+# Pandas is a software library written for the Python programming language for data manipulation and analysis.
+import pandas as pd
+# NumPy is a library for the Python programming language, adding support for large, multi-dimensional arrays and matrices, along with a large collection of high-level mathematical functions to operate on these arrays
+import numpy as np
+# Matplotlib is a plotting library for python and pyplot gives us a MatLab like plotting framework. We will use this in our plotter function to plot data.
+import matplotlib.pyplot as plt
+#Seaborn is a Python data visualization library based on matplotlib. It provides a high-level interface for drawing attractive and informative statistical graphics
+import seaborn as sns
+# Preprocessing allows us to standarsize our data
+from sklearn import preprocessing
+# Allows us to split our data into training and testing data
+from sklearn.model_selection import train_test_split
+# Allows us to test parameters of classification algorithms and find the best one
+from sklearn.model_selection import GridSearchCV
+# Logistic Regression classification algorithm
+from sklearn.linear_model import LogisticRegression
+# Support Vector Machine classification algorithm
+from sklearn.svm import SVC
+# Decision Tree classification algorithm
+from sklearn.tree import DecisionTreeClassifier
+# K Nearest Neighbors classification algorithm
+from sklearn.neighbors import KNeighborsClassifier
+
+def plot_confusion_matrix(y,y_predict):
+    "this function plots the confusion matrix"
+    from sklearn.metrics import confusion_matrix
+
+    cm = confusion_matrix(y, y_predict)
+    ax= plt.subplot()
+    sns.heatmap(cm, annot=True, ax = ax); #annot=True to annotate cells
+    ax.set_xlabel('Predicted labels')
+    ax.set_ylabel('True labels')
+    ax.set_title('Confusion Matrix'); 
+    ax.xaxis.set_ticklabels(['did not land', 'land']); ax.yaxis.set_ticklabels(['did not land', 'landed']) 
+    plt.show() 
+
+import io
+
+URL1 = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/dataset_part_2.csv"
+#resp1 = fetch(URL1)
+#text1 = io.BytesIO((resp1.arrayBuffer()).to_py())
+data = pd.read_csv(URL1)
+URL2 = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/dataset_part_3.csv'
+#resp2 = await fetch(URL2)
+#text2 = io.BytesIO((await resp2.arrayBuffer()).to_py())
+X = pd.read_csv(URL2)
+
+
+print(data)
+print(X)
+
+# TASK 1
+# Create a NumPy array from the column <code>Class</code> in <code>data</code>, by applying the method <code>to_numpy()</code> then
+# assign it to the variable <code>Y</code>,make sure the output is a  Pandas series (only one bracket df\['name of  column']).
+
+Y = data['Class']# 
+print(Y)
+
+# TASK  2
+# Standardize te data in <cod>X</code> then reassto the va<code>X</code>sing the transform provided below.
+
+transformer = preprocessing.StandardScaler()
+transformer.fit(X)
+X = transformer.transform(X)
+print(X)
+
+# We split the data into training and testing data using the function  train_test_split.   
+# The training data is divided into validation data, a second set used for training  data; 
+# then the models are trained and hyperparameters are selected using the function GridSearchCV.
+
+# TASK  3
+# Use the function train_test_split to split the data X and Y into training and test data. 
+# Set the parameter test_size to  0.2 and random_state to 2. The training data and test data should be assigned to the following labels.
+
+# X_train, X_test, Y_train, Y_test
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 2)
+
+print('Y Test :', Y_test.count(), '\n', Y_test.shape)
+
+# TASK  4
+# Create a logistic regression object then create a GridSearchCV object logreg_cv with cv = 10.
+# Fit the object to find the best parameters from the dictionary parameters.
+
+parameters ={'C':[0.01,0.1,1],
+             'penalty':['l2'],
+             'solver':['lbfgs']}
+parameters ={"C":[0.01,0.1,1],'penalty':['l2'], 'solver':['lbfgs']}     # l1 lasso l2 ridge
+lr=LogisticRegression()
+logreg_cv = GridSearchCV(lr, parameters)
+logreg_cv.fit(X_train, Y_train)
+
+# We output the GridSearchCV object for logistic regression. 
+# We display the best parameters using the data attribute best_params\_ and 
+# the accuracy on the validation data using the data attribute best_score\_.
+print("LogRegression: tuned hpyerparameters :(best parameters) ",logreg_cv.best_params_)
+print("Log Regression accuracy :",logreg_cv.best_score_)
+
+# TASK  5
+# Calculate the accuracy on the test data using the method score:
+print('Log Regression Score : ',logreg_cv.score(X_test, Y_test))
+
+# Lets look at the confusion matrix:  
+yhat=logreg_cv.predict(X_test)
+plot_confusion_matrix(Y_test,yhat)
+
+# Examining the confusion matrix, we see that logistic regression can distinguish between the different classes.  
+# We see that the major problem is false positives.
+             
+# TASK  6
+# Create a support vector machine object then 
+# create a GridSearchCV object svm_cv with cv - 10.  
+# Fit the object to find the best parameters from the dictionary parameters.
+parameters = {'kernel':('linear', 'rbf','poly','rbf', 'sigmoid'),
+              'C': np.logspace(-3, 3, 5),
+              'gamma':np.logspace(-3, 3, 5)}
+svm = SVC()
+svm_cv = GridSearchCV(svm, parameters)
+svm_cv.fit(X_train, Y_train)
+
+print("SVM tuned hyperparameters :(best parameters) ",svm_cv.best_params_)
+print("SVM accuracy :",svm_cv.best_score_)
+yhat=svm_cv.predict(X_test)
+plot_confusion_matrix(Y_test,yhat)
+
+# TASK  7
+# Calculate the accuracy on the test data using the method score:
+print('SVM Score : ',svm_cv.score(X_test, Y_test))
+
+# TASK  8
+# Create a decision tree classifier object then create a GridSearchCV object tree_cv with cv = 10.  
+# Fit the object to find the best parameters from the dictionary parameters.
+parameters = {'criterion': ['gini', 'entropy'],
+     'splitter': ['best', 'random'],
+     'max_depth': [2*n for n in range(1,10)],
+     'max_features': ['auto', 'sqrt'],
+     'min_samples_leaf': [1, 2, 4],
+     'min_samples_split': [2, 5, 10]}
+
+tree = DecisionTreeClassifier()
+tree_cv = GridSearchCV(tree, parameters)
+tree_cv.fit(X_train, Y_train)
+
+print("Tree tuned hyperparameters :(best parameters) ",tree_cv.best_params_)
+print("accuracy :",tree_cv.best_score_)
+
+## TASK  9
+print('Tree Score : ',tree_cv.score(X_test, Y_test))
+yhat=tree_cv.predict(X_test)
+#plot_confusion_matrix(Y_test,yhat)
+
+## Task 10
+# Create a k nearest neighbors object then create a GridSearchCV object knn_cv with cv = 10. 
+# Fit the object to find the best parameters from the dictionary parameters.
+parameters = {'n_neighbors': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+              'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
+              'p': [1,2]}
+
+KNN = KNeighborsClassifier()
+
+knn_cv = GridSearchCV(KNN, parameters)
+knn_cv.fit(X_train, Y_train)
+
+print("tuned hyperparameters :(best parameters) ",knn_cv.best_params_)
+print("accuracy :",knn_cv.best_score_)
+
+## TASK 11
+print('KNN Score : ',knn_cv.score(X_test, Y_test))
+yhat=knn_cv.predict(X_test)
+plot_confusion_matrix(Y_test,yhat)
+
+results = {'Log Regression Score' : logreg_cv.score(X_test, Y_test),
+           'SVM Score ' : svm_cv.score(X_test, Y_test),
+           'Tree Score ': tree_cv.score(X_test, Y_test),
+           'KNN Score ': knn_cv.score(X_test, Y_test)     
+           }
+max_value = max(results, key = results.get)
+print(max_value)
+"""
+Have you told readers, at the outset, what they might gain by reading your paper?
+
+Have you made the aim of your work clear?
+
+Have you explained the significance of your contribution?
+
+Have you set your work in the appropriate context by giving sufficient background (including a complete set of relevant references) to your work?
+
+Have you addressed the question of practicality and usefulness?
+
+Have you identified future developments that might result from your work?
+
+Have you structured your paper in a clear and logical fashion?
+
+"""
